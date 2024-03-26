40eda5ccc53325457bff2f3a9775ba7891971d9b
fkamag <fabiokamagian@gmail.com>
2024-03-25 23:07:18 -0300

    starting project

---
+++ src/main/java/br/com/kamatech/searchbycep/SearchByCepApplication.java
@@ -0,0 +1,13 @@
+package br.com.kamatech.searchbycep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchByCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchByCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kamatech/searchbycep/SearchByCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kamatech.searchbycep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchByCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
